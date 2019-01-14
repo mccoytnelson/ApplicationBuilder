75e56d7ee891c60f2379ef86e00edb442c10af86
McCoy Nelson <mccoytnelson@gmail.com>
2019-01-13 20:20:46 -0700

    serverlaunch

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express()
 
 let { SERVER_PORT, SECRET, CONNECTION_STRING,DEV } = process.env
 app.use(express.json())
-// app.use( express.static( `${__dirname}/../build` ) );
+app.use( express.static( `${__dirname}/../build` ) );
 
 app.use(session({
     secret: SECRET,
